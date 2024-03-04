57ff3d4a05b26d280e7c13b913bb251a01f8db91
DESKTOP-GHV8SML\eeist <eeistcool@gmail.com>
2024-03-04 13:00:56 +0100

    window :)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
-public class Main {
+import javax.swing.*;
+
+public class Main extends JFrame {
+
+    public Main() {
+        init();
+    }
+
+    private void init() {
+        setTitle("FractalGen");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setBounds(5, 5, 500, 500);
+        setVisible(true);
+    }
 
     public static void main (String[] args) {
-
+        Main mn = new Main();
     }
 
 }
